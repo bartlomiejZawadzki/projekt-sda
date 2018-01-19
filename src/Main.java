dce5d7060353a54747c2ca9e0b65919dec98176f
bartlomiejZawadzki <zawadzki.brt@gmail.com>
2018-01-19 18:37:43 +0100

    Dodanie Logowanie

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("zmieniony tekst");
+
+        System.out.println("logowanie");
 
         System.out.println("test2");
     }
